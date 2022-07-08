29bdaf68fc35b102887799da176d45281be869e3
xuyongkang <2561258938@qq.com>
2022-07-08 11:47:09 +0800

    7

--- src/main.go
+++ src/main.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("test git")
-	fmt.Println("this is 3 commit")
+
 	fmt.Println("this is 4 commit")
 	fmt.Println("this is 5 commit")
 	fmt.Println("this is 6 commit")
+	fmt.Println("this is 7 commit")
 
 }
